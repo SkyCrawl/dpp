0d2b845417092b9dca13cd7f86270aaacfe4e921
SkyCrawl <smolikj@e-trends.cz>
2016-04-03 17:17:20 +0200

    Adding a test for Option

---
+++ 02-api-test/TestOption.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using Ini.Configuration;
+using Ini.Exceptions;
+using Ini.Configuration.Elements;
+
+namespace ApiTest
+{
+	/*
+	 * HELPFUL LINKS:
+	 * - http://www.nunit.org/index.php?p=attributes&r=2.6.4
+	 * - http://www.nunit.org/index.php?p=constraintModel&r=2.6.4
+	 */
+
+	[TestFixture]
+	public class TestOption
+	{
+		private Option option;
+
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			option = new Option("some-identifier", typeof(bool));
+		}
+
+		[Test, ExpectedException(typeof(InvariantBrokenException))]
+		public void TestInternalObserver()
+		{
+			option.Elements.Add(new StringElement("The internal observer should throw an exception because we're trying to add an element of a different type."));
+		}
+
+		[Test, ExpectedException(typeof(InvalidOperationException))]
+		public void TestSingleElementConversionOnEmptyOption()
+		{
+			option.GetSingleValue<bool>();
+		}
+
+		[Test]
+		[TestCase(TestName = "TestSingleElementConversion (success)")]
+		[TestCase(TestName = "TestSingleElementConversion (failure)", ExpectedException = typeof(InvalidOperationException))]
+		public void TestSingleElementConversion()
+		{
+			option.Elements.Add(new BooleanElement(true));
+			option.GetSingleValue<bool>();
+		}
+	}
+}
---
+++ 02-api-test/TestYAML.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NUnit.Framework;
+using YamlDotNet.Serialization;
+using System.IO;
+using Ini.Schema.Elements;
+
+namespace ApiTest
+{
+	[TestFixture()]
+	public class TestYAML
+	{
+        [Test()]
+        public void TestYAMLDeserialization()
+        {
+            //var spec = new ConfigSpec("Test origin");
+
+            //var section = new SectionSpec { Identifier = "Section 1", Description = "Section commentary", IsMandatory = true };
+            //spec.Sections.Add(section);
+
+            //var stringOption = new StringOptionSpec { Identifier = "String option", Description = "Option commentary", HasSingleValue = true, IsMandatory = true };
+            //section.Options.Add(stringOption);
+
+            //var enumOption = new EnumOptionSpec { Identifier = "Enum option", HasSingleValue = false, IsMandatory = false };
+            //enumOption.AllowedValues.Add("Value1");
+            //enumOption.AllowedValues.Add("Value2");
+            //enumOption.DefaultValues.Add("Value1");
+            //enumOption.DefaultValues.Add("Value1");
+            //section.Options.Add(enumOption);
+
+            //var serializer = new YamlDotNet.Serialization.Serializer(SerializationOptions.EmitDefaults | SerializationOptions.Roundtrip);
+            //var writer = new StringWriter();
+            //serializer.Serialize(writer, spec);
+
+            //var specString = writer.ToString();
+
+            var file = File.OpenText("YamlExample.txt");
+            var specString = file.ReadToEnd();
+                        
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
+            deserializer.TypeResolvers.Insert(0, new TypeResolver());
+
+            var reader = new StringReader(specString);
+            var deserializedSpec = deserializer.Deserialize<ConfigSpec>(reader);
+        }
+	}
+
+    class TypeResolver : INodeTypeResolver
+    {
+        #region INodeTypeResolver Members
+
+        public bool Resolve(YamlDotNet.Core.Events.NodeEvent nodeEvent, ref Type currentType)
+        {
+            switch(nodeEvent.Tag)
+            {
+                case "!String":
+                    currentType = typeof(StringOptionSpec);
+                    return true;
+                case "!Enum":
+                    currentType = typeof(EnumOptionSpec);
+                    return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
