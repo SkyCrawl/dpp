5a2ce55a7e7d34eccfe1e9ca6314e8417a2d0531
JanVana <jan.vana@outlook.com>
2016-05-05 15:32:17 +0200

    Task 4
    Logování neplatných linků

--- 02-api/EventLoggers/IConfigWriterEventLogger.cs
+++ 02-api/EventLoggers/IConfigWriterEventLogger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Ini;
+using Ini.Configuration.Base;
 
 namespace Ini.EventLoggers
 {
@@ -21,5 +22,13 @@ namespace Ini.EventLoggers
         /// on the currently used <see cref="ConfigWriterOptions"/>.
         /// </summary>
         void ConfigurationNotValid();
+
+        /// <summary>
+        /// One of the links in the configuration was inconsistent with it's origin option.
+        /// </summary>
+        /// <param name="section">The link section</param>
+        /// <param name="option">The link option</param>
+        /// <param name="link">The link instance</param>
+        void LinkInconsistent(string section, string option, ILink link);
     }
 }
